d6206f7264756066455b060ffcbecd4ac304ecd8
SidRepository1 <siddharthbrahambhatt6666@gmail.com>
2025-11-04 23:30:22 +0530

    removed run time env config

--- src/main/java/com/in/controller/UserCtl.java
+++ src/main/java/com/in/controller/UserCtl.java
@@ -3,7 +3,6 @@ package com.in.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,14 +20,6 @@ public class UserCtl {
 	@Autowired
 	private UserService service;
 
-	// #id => parameter passed, for multiple parameter we can use ({#parameter1,
-	// #parameter2}
-	// value => any name you want to specify
-	// condition => any condition you want to execute with parameter
-	// #result => this is result object which we are returning from method
-	// @Cacheput => used with post methods
-	// @Cacheevict => used when, we want to store data into DB, but not into cache
-	@Cacheable(key = "#id", value = "User", condition = "#id<5", unless = "#result.fname=='sid3'")
 	@GetMapping("/getuser/{id}")
 	public Optional<User> getByID(@PathVariable(value = "id") Integer id) {
 		return service.findById(id);
@@ -36,7 +27,6 @@ public class UserCtl {
 
 	@PostMapping("/create")
 	public User createUser(@RequestBody User user) {
-//		logger.debug("inside /create");
 		return service.createUser(user);
 	}
 
